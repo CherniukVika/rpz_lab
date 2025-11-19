7b16f7626769355b7176b2bde6a0c2a06776e0a4
CherniukVika <ch.vika2006@gmail.com>
2025-11-19 16:17:21 +0200

    add tests for EchoServer

---
+++ EchoServerTests/Server/EchoServerTests.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Sockets;
+using EchoServer.Interfaces;
+using EchoServer.Server;
+using Moq;
+using NUnit.Framework;
+
+namespace EchoServerTests.Server
+{
+    [TestFixture]
+    public class EchoServerTests
+    {
+        private Mock<ITcpListenerWrapper> _mockListener;
+        private Mock<IClientHandler> _mockHandler;
+        private EchoServer.Server.EchoServer _server;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockListener = new Mock<ITcpListenerWrapper>();
+            _mockHandler = new Mock<IClientHandler>();
+            _server = new EchoServer.Server.EchoServer(_mockListener.Object, _mockHandler.Object);
+        }
+
+        [Test]
+        public void Constructor_ThrowsArgumentNullException_WhenListenerIsNull()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new EchoServer.Server.EchoServer(null, _mockHandler.Object));
+        }
+
+        [Test]
+        public void Constructor_ThrowsArgumentNullException_WhenHandlerIsNull()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new EchoServer.Server.EchoServer(_mockListener.Object, null));
+        }
+
+        [Test]
+        public async Task StartAsync_StartsListener()
+        {
+            // Arrange
+            _mockListener.Setup(l => l.AcceptTcpClientAsync())
+                .ThrowsAsync(new ObjectDisposedException("test"));
+
+            // Act
+            await _server.StartAsync();
+
+            // Assert
+            _mockListener.Verify(l => l.Start(), Times.Once);
+        }
+
+        [Test]
+        public void Stop_StopsListener()
+        {
+            // Act
+            _server.Stop();
+
+            // Assert
+            _mockListener.Verify(l => l.Stop(), Times.Once);
+        }
+
+        [Test]
+        public async Task StartAsync_HandlesObjectDisposedException()
+        {
+            // Arrange
+            _mockListener.Setup(l => l.AcceptTcpClientAsync())
+                .ThrowsAsync(new ObjectDisposedException("listener"));
+
+            // Act & Assert
+            Assert.DoesNotThrowAsync(async () => await _server.StartAsync());
+        }
+    }
+}
