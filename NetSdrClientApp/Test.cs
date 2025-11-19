882911ed67411b3b7cd6be98b84c35c523150686
CherniukVika <ch.vika2006@gmail.com>
2025-11-19 15:24:57 +0200

    failed UI_ShouldNotDependOnInfrastructureDirectly

---
+++ NetSdrClientApp/Test.cs
@@ -0,0 +1,12 @@
+﻿using EchoServer;
+
+namespace NetSdrClientApp
+{
+    public class Test
+    {
+       public void CreateServer()
+        {
+            var server = new EchoServer.EchoServer(5000); // ось тут залежність на інфраструктуру
+        }
+    }
+}
---
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -0,0 +1,22 @@
+﻿using NetArchTest.Rules;
+
+namespace NetSdrClientAppTests
+{
+    public class ArchitectureTests
+    {
+        [Test]
+        public void UI_ShouldNotDependOnInfrastructureDirectly()
+        {
+            var assembly = typeof(NetSdrClientApp.NetSdrClient).Assembly;
+
+            var result = Types.InAssembly(assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp")
+                .ShouldNot()
+                .HaveDependencyOn("EchoServer")
+                .GetResult();
+
+            Assert.IsTrue(result.IsSuccessful);
+        }
+    }
+}
